f863be74539c9a4286cdd2f1f9ddc7d6f96163e8
lequanganit <lequanganit@gmail.com>
2025-11-16 22:23:53 +0700

    update b1

---
+++ ecourseapis/courses/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from ckeditor.fields import RichTextField
+
+class User(AbstractUser):
+    pass
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(models.Model):
+    #id tu co
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+
+class Course(BaseModel):
+    subject = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='courses/%Y/%m', null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
+
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=255)
+    content = RichTextField()
+    image = models.ImageField(upload_to='lesson/%Y/%m', null=True)
+    course = models.ForeignKey(Course, on_delete=models.RESTRICT)
+    def __str__(self):
+        return self.subject
+    class Meta:
+        unique_together = ('subject', 'course')
+
+
+
